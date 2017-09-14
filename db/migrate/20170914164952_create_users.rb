fb6f6353de15e8888df9f00a5891483f2d9683a7
Thomas <thomasmbise@gmail.com>
2017-09-14 17:31:14 -0400

    Create user migration

---
+++ db/migrate/20170914164952_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, { null: false, limit: 10, unique: true, index: { unique: true } }
+      t.string :email, { index: { unique: true }, null: false, limit: 20 }
+      t.string :password_digest, { null: false, length: { minimum: 7, maximum: 50 } }
+      t.integer :budget_amount, { greater_than_or_equal_to: 0, allow_blank: true }
+      t.time :pay_period, { greater_than_or_equal_to: 0, allow_blank: true }
+
+      t.timestamps
+    end
+  end
+end
